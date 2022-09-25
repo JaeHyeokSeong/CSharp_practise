c01a646b35666872b0a7d015649c2f911b85d5e9
JaeHyeokSeong <seong5763@gmail.com>
2022-09-26 04:55:50 +1000

    upload code

---
+++ source_code/LINQ_Practise1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace LINQ_Practise1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string userName = "", password = "";
+        bool loginStatus = false;
+        Console.WriteLine("Please enter username and password");
+        Console.Write("Username: ");
+        userName = Console.ReadLine();
+        Console.Write("Password: ");
+        password = Console.ReadLine();
+
+        StreamReader sr = new StreamReader("login.txt");
+        string? line = sr.ReadLine();
+        string[] userInfo;
+        while(line != null)
+        {
+            userInfo = line.Split(',');
+            string readUsername = userInfo[0].Trim();
+            string readPassword = userInfo[1].Trim();
+            if(userName == readUsername && password == readPassword)
+            {
+                loginStatus = true;
+                break;
+            }
+            line = sr.ReadLine();
+        }
+        sr.Close();
+
+        if (loginStatus)
+        {
+            Console.WriteLine("\nLogin Success");
+        }
+        else
+        {
+            Console.WriteLine("\nLogin Fail");
+        }
+    }
+}
